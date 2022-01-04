8d9f3c973fc825bbb3c0ee7387c305548ddebb1c
rita.dongare <rita.dongare@collabera.com>
2022-01-04 15:47:28 +0530

    java-Variables:Using:unit:test

---
+++ java-simple-app/src/test/java/LearningVariablesUsingUnitTests.java
@@ -0,0 +1,21 @@
+
+    import org.junit.jupiter.api.DisplayName;
+     import org.junit.jupiter.api.Test;
+      import org.junit.jupiter.api.Assertions;
+    public class LearningVariablesUsingUnitTests {
+
+        @DisplayName("Check variable values are correct")
+        @Test  //this annotation
+        void learnTheVariablesValidCase(){
+            int osNum=12;
+            int ExpectedValue=12;
+            Assertions.assertEquals(expectedValue, osNum);
+        }
+    @DisplayName("Check variable values are incorrect")
+        @Test
+        void learnTheVariablesInvalidCase(){
+            int osNum=12;
+            int expectedValue=13;
+        Assertions.assertNotEquals(expectedValue, osNum);
+    }
+}
