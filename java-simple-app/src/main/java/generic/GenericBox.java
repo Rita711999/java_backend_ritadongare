c1270c6457c88eaeb43e795f8ef32c6392a116eb
rita.dongare <rita.dongare@collabera.com>
2022-01-05 14:17:21 +0530

    generic

---
+++ java-simple-app/src/main/java/generic/GenericBox.java
@@ -0,0 +1,13 @@
+package generic;
+
+public class GenericBox<T> {
+    private T content;
+
+    public T getContent() {
+        return content;
+    }
+
+    public void setContent(T content) {
+        this.content = content;
+    }
+}
---
+++ java-simple-app/src/main/java/generic/NormalBox.java
@@ -0,0 +1,13 @@
+package generic;
+
+public class NormalBox {
+    private Object content;
+
+    public Object getContent() {
+        return content;
+    }
+
+    public void setContent(Object content) {
+        this.content = content;
+    }
+}
