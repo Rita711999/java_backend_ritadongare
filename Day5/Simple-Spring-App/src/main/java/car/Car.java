4bc6ce78a417651673a94b33cdf0b9ca170299cb
rita.dongare <rita.dongare@collabera.com>
2022-01-07 17:30:18 +0530

    spring: dependancy injection intro

--- Day5/Simple-Spring-App/src/main/java/car/Car.java
+++ Day5/Simple-Spring-App/src/main/java/car/Car.java
@@ -1,14 +1,13 @@
 package car;
 
-public class Car {
-    private String model;
+    public class Car {
+        private int speed;
 
-    public String getModel(){
-        return  model;
-    }
+        public int getSpeed() {
+            return speed;
+        }
 
-    public void setModel(String model)
-    {
-        this.model=model;
+        public void setSpeed(int speed) {
+            this.speed = speed;
+        }
     }
-}
