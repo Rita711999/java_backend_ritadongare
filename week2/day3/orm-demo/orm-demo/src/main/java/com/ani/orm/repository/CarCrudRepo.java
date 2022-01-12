893fc242ebdaf9d8ff4d580c42733d5fa49b8f83
rita.dongare <rita.dongare@collabera.com>
2022-01-12 22:33:49 +0530

    Spring data :crud opration

---
+++ week2/day3/orm-demo/orm-demo/src/main/java/com/ani/orm/repository/CarCrudRepo.java
@@ -0,0 +1,12 @@
+package com.ani.orm.repository;
+import com.ani.orm.domain.Car;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CarCrudRepo extends CrudRepository<Car, Long > {
+    @Override
+    List<Car> findAll();
+}
