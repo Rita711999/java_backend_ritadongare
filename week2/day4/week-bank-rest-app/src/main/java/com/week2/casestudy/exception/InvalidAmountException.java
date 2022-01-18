7f184dc2895e85844dcf671d79d75fe63d375036
rita.dongare <rita.dongare@collabera.com>
2022-01-18 12:04:07 +0530

    bank opration: rest api

---
+++ week2/day4/week-bank-rest-app/src/main/java/com/week2/casestudy/exception/InvalidAmountException.java
@@ -0,0 +1,8 @@
+package com.week2.casestudy.exception;
+
+public class InvalidAmountException extends RuntimeException {
+
+    public InvalidAmountException(String message) {
+        super(message);
+    }
+}
